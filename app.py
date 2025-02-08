633a0550b004cd6e2065b93126d0a20307ab7428
Gurudev <gurudevsingla@gmail.com>
2025-02-08 16:55:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ MODEL_PATH = "model/xray_model2.keras"
 SAMPLE_IMAGES_PATH = "sample_images/"
 
 # Sidebar for user input
-st.sidebar.title("X-ray Image Predictor")
+st.sidebar.title("Pneumonia Detection")
 
 option = st.sidebar.radio("Upload or Select an Image", ('Upload', 'Select from Sample'))
 
@@ -32,7 +32,7 @@ elif option == 'Select from Sample':
         sample_images_path = SAMPLE_IMAGES_PATH + 'non_pneumonic'
 
     images = os.listdir(sample_images_path)
-    selected_image = st.selectbox('Select an Image', images, index=0)
+    selected_image = st.selectbox('Select an Image', images, index=1)
     image_path = sample_images_path + "/" + selected_image
 
     image = Image.open(image_path)

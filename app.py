1912ea7f6833c832814ffe0342e38fd6a6b8295b
Gurudev <gurudevsingla@gmail.com>
2025-02-08 17:29:08 +0530

    cache_code_removed

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ import os
 import warnings
 warnings.filterwarnings('ignore')
 
-st.cache_data.clear()
-st.cache_resource.clear()
-
 # Define paths
 MODEL_PATH = "model/xray_model2.keras"
 SAMPLE_IMAGES_PATH = "sample_images/"
